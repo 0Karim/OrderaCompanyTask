986dafd0442c14f2846fbcf242f7530cd6bd9a12
Karim <karimalaa298@gmail.com>
2019-02-10 21:21:44 +0200

    Add project files.

---
+++ OrderaTaskVersion2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OrderaTaskVersion2.Startup))]
+namespace OrderaTaskVersion2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
